5dda4938152550005747def58f1014aaa6750ad0
frtamba <121829150+frtamba@users.noreply.github.com>
2023-02-28 23:56:25 +0100

    Create an Upload Form

--- myapp.js
+++ myapp.js
@@ -1,6 +1,11 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('rtmp://195.90.214.31/:1935/live/stream!');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('<h2>rtmp://195.90.214.31/:1935/live/stream!</h2>');
+  res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+  res.write('<input type="file" name="filetoupload"><br>');
+  res.write('<input type="submit">');
+  res.write('</form>');
+  return res.end();
 }).listen(8080);
