dccd161bad2c92cbafb7a8b6afcb20b507de6687
frtamba <121829150+frtamba@users.noreply.github.com>
2023-03-01 00:00:17 +0100

    Parse the Uploaded File

--- myapp.js
+++ myapp.js
@@ -1,6 +1,14 @@
 var http = require('http');
+var formidable = require('formidable');
 
 http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      res.write('File uploaded');
+      res.end();
+    });
+  }else {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('<h2>rtmp://195.90.214.31/:1935/live/stream!</h2>');
   res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
@@ -8,4 +16,5 @@ http.createServer(function (req, res) {
   res.write('<input type="submit">');
   res.write('</form>');
   return res.end();
+}
 }).listen(8080);
