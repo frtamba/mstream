2397ca881b572eff9b81ec11daf484efb81008af
frtamba <121829150+frtamba@users.noreply.github.com>
2023-03-01 00:16:24 +0100

    code updated

--- myapp.js
+++ myapp.js
@@ -6,22 +6,21 @@ http.createServer(function (req, res) {
   if (req.url == '/fileupload') {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
-      
       var oldpath = files.filetoupload.filepath;
-      var newpath = 'upload/' + files.filetoupload.originalFilename;
+      var newpath = 'uploads/' + files.filetoupload.originalFilename;
       fs.rename(oldpath, newpath, function (err) {
         if (err) throw err;
-      
-      res.write('File uploaded');
-      res.end();
-    });
-  }else {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('<h2>rtmp://195.90.214.31/:1935/live/stream!</h2>');
-  res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-  res.write('<input type="file" name="filetoupload"><br>');
-  res.write('<input type="submit">');
-  res.write('</form>');
-  return res.end();
-}
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<h2>rtmp://195.90.214.31/:1935/live/stream!</h2>');
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
 }).listen(8080);
