2d71a3549768bfdc08444a45aac2c2007c6a8daf
frtamba <121829150+frtamba@users.noreply.github.com>
2023-03-03 16:16:14 +0100

    get form data

--- myapp.js
+++ myapp.js
@@ -20,14 +20,5 @@ http.createServer(function (req, res) {
         res.end();
       });
  });
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h2>rtmp://195.90.214.31/:1935/live/stream!</h2>');
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="text" name="mlink"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
-  }
+  } 
 }).listen(8080);
